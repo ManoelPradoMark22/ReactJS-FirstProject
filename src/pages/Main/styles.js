3f0370c26a4c9db43a32fb3cd593fde5039b3fec
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-09-24 09:20:55 -0300

    Estilizando Página Main

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); /*20 de blur */
+  padding: 30px;
+  margin: 80px auto; /*80 cima e 80 baixo, auto deixa
+  centralizado horizontalmente na max-wdth */
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1; /*vai ocupar todo espaco possivel! */
+    border: 1px solid #eee;
+    padding: 10px 15px; /*10px vertical e 15 px horizontal */
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
